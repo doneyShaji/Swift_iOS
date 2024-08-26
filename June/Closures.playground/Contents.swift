323ddf7eff81b2beda6bad07c417b99d3d1162f3
doneyShaji <doney.shaji@people10.com>
2024-08-26 18:32:34 +0530

    Closures & Completion Handlers

--- June/Closures.playground/Contents.swift
+++ June/Closures.playground/Contents.swift
@@ -1,11 +1,60 @@
 import UIKit
 
-func grabLunch(message: String, search: ()->()) {
-   print(message)
-   search()
+// How Closures Work?
+var doubleAmount: (Int) -> (Int) = { number in
+        return number + number
+}
+let result = doubleAmount(5)
+print(result)
+
+
+// Consice Swift Closure Example
+
+    var array = [6, 4, 6, 2, 8, 3, 0]
+    var sorted = array.sorted(by: < )
+    print(sorted)
+
+    // Different ways to define the above closures
+        //A) First, when we expand < it becomes this:
+        var sortedA = array.sorted(by: { (s1: Int, s2: Int) -> Bool in
+            return s1 < s2
+        })
+        print(sortedA)
+
+        //B) Another way to write the same expression
+        var sortedB = array.sorted(by: {s1, s2 in
+            s1 < s2
+        })
+        print(sortedB)
+
+        //C) With shorthand operators
+        var sortedC = array.sorted(by: {$0 < $1} )
+        print(sortedC)
+
+        //D) With trailing closure syntax
+        var sortedD = array.sorted { $0 < $1 }
+        print(sortedD)
+
+        //E) And finally, you can also just use the < operator as a function:
+        var sortedE = array.sorted(by: < )
+        print(sortedE)
+
+
+// Completion handler
+
+let myCompletionHandler: (String) -> () = { done in
+        print("The status is \(done)")
 }
 
-// use of trailing closure
-grabLunch(message:  {
-  print("Alfredo's Pizza: 2 miles away")
+//Function Block
+
+func checkCompletionHandler(foreign completion: (String) -> ()){
+    for i in 1...5 {
+        print(i)
+    }
+    completion("Done")
 }
+
+//Calling
+
+checkCompletionHandler(foreign: myCompletionHandler)
